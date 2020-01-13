1c3e2d4060d36eb82d76919b811665ab66b310bd
KamranV21 <kamranv21@gmail.com>
2020-01-13 19:14:18 +0400

    Image upload

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,51 +1,52 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const upload = require('../../middleware/upload')
 
-const Post = require('../../models/Post');
+const Post = require('../../models/Post')
 
 // @route GET api/posts
 // @desc  Posts route
 // @acess Public
-router.get('/', (req, res) => res.send('Posts route...'));
+router.get('/', (req, res) => res.send('Posts route...'))
 
 // @route POST api/posts
 // @desc  Create post
 // @acess Private
 router.post(
   '/',
-  passport.authenticate('jwt', { session: false }),
+  [passport.authenticate('jwt', { session: false }), upload.single('photo')],
   async (req, res) => {
-    const { city, address, rooms, sqm, description, price } = req.body;
+    const { city, address, rooms, sqm, description, price } = req.body
 
     try {
       // Build post object
-      const postFields = {};
-      postFields.generalInfo = {};
-      postFields.estate = {};
-      postFields.priceInfo = {};
+      const postFields = {}
+      postFields.generalInfo = {}
+      postFields.estate = {}
+      postFields.priceInfo = {}
 
-      postFields.user = req.user.id;
-      fillObjectFields(postFields, { description });
-      fillObjectFields(postFields.generalInfo, { city, address });
-      fillObjectFields(postFields.estate, { rooms, sqm });
-      fillObjectFields(postFields.priceInfo, { price });
+      postFields.user = req.user.id
+      fillObjectFields(postFields, { description })
+      fillObjectFields(postFields.generalInfo, { city, address })
+      fillObjectFields(postFields.estate, { rooms, sqm })
+      fillObjectFields(postFields.priceInfo, { price })
 
-      const post = new Post(postFields);
-      await post.save();
+      const post = new Post(postFields)
+      await post.save()
 
-      res.json(post);
+      res.json(post)
     } catch (error) {
-      console.error(error.message);
-      return res.status(500).send('Server error...');
+      console.error(error.message)
+      return res.status(500).send('Server error...')
     }
   }
-);
+)
 
 const fillObjectFields = (object, fields) => {
   Object.keys(fields).map(key => {
-    if (fields[key]) object[key] = fields[key];
-  });
-};
+    if (fields[key]) object[key] = fields[key]
+  })
+}
 
-module.exports = router;
+module.exports = router
